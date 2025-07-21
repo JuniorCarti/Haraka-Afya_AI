4adafa4ee39615f4dac55eaa89ca1df1425808db
JuniorCarti <ridgejunior204@gmail.com>
2025-07-21 15:52:29 +0300

    feat: add OverviewPage screen with basic UI

---
+++ lib/features/overview_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OverviewPage extends StatelessWidget {
+  const OverviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFFE6F6EC),
+      child: const Center(
+        child: Text('Overview Content'),
+      ),
+    );
+  }
+}
