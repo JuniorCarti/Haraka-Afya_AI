6ab29172ae8f8b382cbceefb5c6fefda23186a07
JuniorCarti <ridgejunior204@gmail.com>
2025-07-23 18:04:02 +0300

    feat: Add UserService for profile operations

---
+++ lib/services/user_service.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/user_profile.dart';
+
+class UserService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<void> updateProfile(UserProfile profile) async {
+    await _db.collection('users').doc(profile.uid).update({
+      'firstName': profile.firstName,
+      'lastName': profile.lastName,
+      'phoneNumber': profile.phoneNumber,
+      'age': profile.age,
+    });
+  }
+}
