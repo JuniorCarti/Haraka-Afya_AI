e1075710057d3eff229503cc29c361c737fd3fa5
JuniorCarti <ridgejunior204@gmail.com>
2025-10-02 15:37:03 +0300

    feat(voice-room): add members list and available games to VoiceRoomScreen state

--- lib/screens/voice_room_screen.dart
+++ lib/screens/voice_room_screen.dart
@@ -7,3 +7,13 @@ class VoiceRoomScreen extends StatefulWidget {
   @override
   State<VoiceRoomScreen> createState() => _VoiceRoomScreenState();
 }
+class _VoiceRoomScreenState extends State<VoiceRoomScreen> {
+  final List<RoomMember> _members = [];
+  final List<RoomGame> _availableGames = [
+    RoomGame('Pool Billiard', Icons.sports, Colors.green),
+    RoomGame('Sudoku', Icons.grid_4x4, Colors.blue),
+    RoomGame('Roll Dice', Icons.casino, Colors.orange),
+    RoomGame('Chess', Icons.extension, Colors.brown),
+    RoomGame('Cards', Icons.style, Colors.red),
+    RoomGame('Word Game', Icons.text_fields, Colors.purple),
+  ];
