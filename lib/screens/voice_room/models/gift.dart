e90037834fcbdbaf919c0d517f0abf8ddacc18b5
JuniorCarti <ridgejunior204@gmail.com>
2025-10-02 17:42:37 +0300

    feat: add Gift model for gifting system
    - Create Gift data class with gift properties
    - Include name, emoji, cost, and color for gift representation
    - Enable gift sending functionality between users

---
+++ lib/screens/voice_room/models/gift.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Gift {
+  final String name;
+  final String emoji;
+  final int cost;
+  final Color color;
+
+  Gift(this.name, this.emoji, this.cost, this.color);
+}
