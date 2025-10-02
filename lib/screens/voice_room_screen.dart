227274e5658b6a7e3de3d461fdc5bbfc11f5bf35
JuniorCarti <ridgejunior204@gmail.com>
2025-10-02 15:36:17 +0300

    feat(voice-room): create VoiceRoomScreen base widget with state setup

---
+++ lib/screens/voice_room_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:haraka_afya_ai/widgets/app_drawer.dart';
+
+class VoiceRoomScreen extends StatefulWidget {
+  const VoiceRoomScreen({super.key});
+
+  @override
+  State<VoiceRoomScreen> createState() => _VoiceRoomScreenState();
+}
