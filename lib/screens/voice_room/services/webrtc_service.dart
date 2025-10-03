2e01233378020926855e62cfbee83e1be9692d8d
JuniorCarti <ridgejunior204@gmail.com>
2025-10-03 23:54:41 +0300

    Scaffold WebRTCService with socket, peer connections, and media stream placeholders

---
+++ lib/screens/voice_room/services/webrtc_service.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class WebRTCService {
+  late IO.Socket _socket;
+  final Map<String, RTCPeerConnection> _peerConnections = {};
+  final Map<String, MediaStream> _remoteStreams = {};
+  MediaStream? _localStream;
+  
